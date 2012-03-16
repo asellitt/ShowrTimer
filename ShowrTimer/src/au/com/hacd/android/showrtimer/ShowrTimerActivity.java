1cfc0ce91782d4e600e9dfc48a2987e256d5220b
Anthony Sellitti <anthony@hacd.com.au>
2012-03-16 21:59:32 +1100

    android projecct structure plus basic layouts

---
+++ ShowrTimer/src/au/com/hacd/android/showrtimer/ShowrTimerActivity.java
@@ -0,0 +1,13 @@
+package au.com.hacd.android.showrtimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowrTimerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
